2ced2a42e23073de90b2e16d4d8d7d1606ba4428
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-12-18 17:33:29 +0530

    r

---
+++ prasanna-11/src/index.js
@@ -0,0 +1,13 @@
+import ReactDom from "react-dom/client"
+import App from './App'
+
+const hw =ReactDom.createRoot(document.getElementById('hw')); 
+hw.render(
+    <>
+    <App/>
+    </>
+)
+
+
+
+
